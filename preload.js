1d438c416b058732f3ede65cc235352a6015cf85
Bean <ihubnetceogeenomc@gmail.com>
2020-01-12 11:41:37 -0800

    Comment Changes

--- preload.js
+++ preload.js
@@ -1,5 +1,6 @@
-// All of the Node.js APIs are available in the preload process.
-// It has the same sandbox as a Chrome extension.
+// All Node APIs are available during preload process
+// Has same sandbox as Chrome extensions
+// Preloads all elements
 window.addEventListener('DOMContentLoaded', () => {
   const replaceText = (selector, text) => {
     const element = document.getElementById(selector)
